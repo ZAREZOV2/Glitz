95a21fd94ff546a50b2a36d3ce07904e7406bc1d
Lucas Moris <lucasmoris522@gmail.com>
2025-05-03 01:25:36 +0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Glitz
+### Install
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ZAREZOV2/Glitz/main/main.lua', true))()
+```
--- main.lua
+++ main.lua
@@ -30,45 +30,107 @@ local Tabs = {
 }
 
 -- Добавление элементов управления
-local Toggle = Tabs.Main:CreateToggle("MyToggle", {Title = "Toggle", Default = false })
+local Toggle = Tabs.Main:CreateToggle(
+    "MyToggle", {
+        Title = "Fly", 
+        Default = false 
+    })
 
 Toggle:OnChanged(function()
-    print("Toggle changed:", Options.MyToggle.Value)
+    local FlyKey = Enum.KeyCode.V 
+    local SpeedKey = Enum.KeyCode.LeftControl 
+     
+    local SpeedKeyMultiplier = 3 
+    local FlightSpeed = 256 
+    local FlightAcceleration = 4 
+    local TurnSpeed = 16 
+      
+    local UserInputService = game:GetService("UserInputService") 
+    local StarterGui = game:GetService("StarterGui") 
+    local RunService = game:GetService("RunService") 
+    local Players = game:GetService("Players") 
+    local User = Players.LocalPlayer 
+    local Camera = workspace.CurrentCamera 
+    local UserCharacter = nil 
+    local UserRootPart = nil 
+    local Connection = nil 
+     
+    workspace.Changed:Connect(function() 
+        Camera = workspace.CurrentCamera 
+    end) 
+     
+    local setCharacter = function(c) 
+        UserCharacter = c 
+        UserRootPart = c:WaitForChild("HumanoidRootPart") 
+    end 
+     
+    User.CharacterAdded:Connect(setCharacter) 
+    if User.Character then 
+        setCharacter(User.Character) 
+    end 
+     
+    local CurrentVelocity = Vector3.new(0,0,0) 
+    local Flight = function(delta) 
+        local BaseVelocity = Vector3.new(0,0,0) 
+        if not UserInputService:GetFocusedTextBox() then 
+            if UserInputService:IsKeyDown(Enum.KeyCode.W) then 
+                BaseVelocity = BaseVelocity + (Camera.CFrame.LookVector * FlightSpeed) 
+            end 
+            if UserInputService:IsKeyDown(Enum.KeyCode.A) then 
+                BaseVelocity = BaseVelocity - (Camera.CFrame.RightVector * FlightSpeed) 
+            end 
+            if UserInputService:IsKeyDown(Enum.KeyCode.S) then 
+                BaseVelocity = BaseVelocity - (Camera.CFrame.LookVector * FlightSpeed) 
+            end 
+            if UserInputService:IsKeyDown(Enum.KeyCode.D) then 
+                BaseVelocity = BaseVelocity + (Camera.CFrame.RightVector * FlightSpeed) 
+            end 
+            if UserInputService:IsKeyDown(Enum.KeyCode.Space) then 
+                BaseVelocity = BaseVelocity + (Camera.CFrame.UpVector * FlightSpeed) 
+            end 
+            if UserInputService:IsKeyDown(SpeedKey) then 
+                BaseVelocity = BaseVelocity * SpeedKeyMultiplier 
+            end 
+        end 
+        if UserRootPart then 
+            local car = UserRootPart:GetRootPart() 
+            if car.Anchored then return end 
+            if not isnetworkowner(car) then return end 
+            CurrentVelocity = CurrentVelocity:Lerp( 
+                BaseVelocity, 
+                math.clamp(delta * FlightAcceleration, 0, 1) 
+            ) 
+            car.Velocity = CurrentVelocity + Vector3.new(0,2,0) 
+            if car ~= UserRootPart then 
+                car.RotVelocity = Vector3.new(0,0,0) 
+                car.CFrame = car.CFrame:Lerp(CFrame.lookAt( 
+                    car.Position, 
+                    car.Position + CurrentVelocity + Camera.CFrame.LookVector 
+                ), math.clamp(delta * TurnSpeed, 0, 1)) 
+            end 
+        end 
+    end
+
+    StarterGui:SetCore("SendNotification",{ 
+        Title = "Fly activated!", 
+        Text = "Press [V] On/Off" 
+    })    
+    
 end)
 
--- Addons:
--- SaveManager (Allows you to have a configuration system)
--- InterfaceManager (Allows you to have a interface managment system)
-
--- Hand the library over to our managers
 SaveManager:SetLibrary(Library)
 InterfaceManager:SetLibrary(Library)
 
--- Ignore keys that are used by ThemeManager.
--- (we dont want configs to save themes, do we?)
 SaveManager:IgnoreThemeSettings()
 
--- You can add indexes of elements the save manager should ignore
 SaveManager:SetIgnoreIndexes{}
 
--- use case for doing it this way:
--- a script hub could have themes in a global folder
--- and game configs in a separate folder per game
 InterfaceManager:SetFolder("FluentScriptHub")
 SaveManager:SetFolder("FluentScriptHub/specific-game")
 
 InterfaceManager:BuildInterfaceSection(Tabs.Settings)
 SaveManager:BuildConfigSection(Tabs.Settings)
 
-
 Window:SelectTab(1)
 
-Library:Notify{
-    Title = "Fluent",
-    Content = "The script has been loaded.",
-    Duration = 8
-}
-
--- You can use the SaveManager:LoadAutoloadConfig() to load a config
--- which has been marked to be one that auto loads!
 SaveManager:LoadAutoloadConfig()
